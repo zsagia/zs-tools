c8be0e09e5672c266aba826c194fc6d27e947c60
zsagia <zsagia@gmail.com>
2021-07-01 15:57:12 +0200

    Init project

---
+++ apps/zs-tools-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zs-tools-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'zs-tools-demo';
+}
