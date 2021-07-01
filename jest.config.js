03c0aa2ea4230bb0ca51646e292552b01fc764dc
zsagia <zsagia@gmail.com>
2021-07-01 16:12:14 +0200

    Generate tools/table lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    projects: ['<rootDir>/apps/zs-tools-demo', '<rootDir>/libs/api'],
+    projects: ['<rootDir>/apps/zs-tools-demo', '<rootDir>/libs/api', '<rootDir>/libs/tools/table'],
 };
---
+++ libs/tools/table/README.md
@@ -0,0 +1,7 @@
+# tools-table
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test tools-table` to execute the unit tests.
