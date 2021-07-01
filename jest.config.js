9f9e86651634b6b018efb87011a603c24729a3a2
zsagia <zsagia@gmail.com>
2021-07-01 16:13:18 +0200

    Generate util/faker lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    projects: ['<rootDir>/apps/zs-tools-demo', '<rootDir>/libs/api', '<rootDir>/libs/tools/table'],
+    projects: [
+        '<rootDir>/apps/zs-tools-demo',
+        '<rootDir>/libs/api',
+        '<rootDir>/libs/tools/table',
+        '<rootDir>/libs/util/faker',
+    ],
 };
---
+++ libs/util/faker/README.md
@@ -0,0 +1,7 @@
+# util-faker
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test util-faker` to execute the unit tests.
